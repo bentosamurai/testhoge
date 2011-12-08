4ba9b0ef2b765d35c0e6c1e4ec3748a7f819e6a3
Jun Fukaya <fukajun.shark@gmail.com>
2011-12-09 00:02:37 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testhoge::Application.config.secret_token = 'b4d5fd58cc925595c4ecc9900238bbe5e5b1c1b1626ea39d42adfd0f0f7e90750eec1b8a4d2b99b73c621891bc0209f4ff5a499e8e60b2bf97dd4ae324bce8f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Testhoge::Application.config.session_store :cookie_store, key: '_testhoge_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Testhoge::Application.config.session_store :active_record_store
